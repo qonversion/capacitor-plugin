6fa69c207af4b048ba599eab830e4b3cd66d4af2
Kamo Spertsyan <spertsiankamo@gmail.com>
2026-01-21 13:16:41 +0300

    Capacitor 8 support

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "QonversionCapacitor",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "QonversionCapacitor",
